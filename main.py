ea2052462e8639357e24cf379e061e3f05d65523
jNathan00 <125561634+jNathan00@users.noreply.github.com>
2023-03-19 14:50:04 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 timetable = []
 
@@ -14,7 +14,7 @@ def index():
         timetable.append({'subject': subject, 'day': day, 'time start': timestart, 'time end': timeend})
     return render_template('index.html', subjects=timetable)
 
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
-
 
