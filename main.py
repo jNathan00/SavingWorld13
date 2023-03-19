5be5c6afc1f33e161d4d81c3b80d695df0223bbe
jNathan00 <125561634+jNathan00@users.noreply.github.com>
2023-03-19 13:25:53 +0000

    .

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 from flask import Flask, render_template, request
-import sqlite3
 
 app = Flask(__name__)
 
-# Route to display the timetable
-@app.route('/timetable', methods=['GET'])
-def timetable():
-    conn = sqlite3.connect('timetable.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM events")
-    events = c.fetchall()
-    conn.close()
-    return render_template('timetable.html', events=events)
+timetable = []
 
-# Route to add an event to the timetable
-@app.route('/add_event', methods=['POST'])
-def add_event():
-    name = request.form['name']
-    start_time = request.form['start_time']
-    end_time = request.form['end_time']
-    date = request.form['date']
-    conn = sqlite3.connect('timetable.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO events (name, start_time, end_time, date) VALUES (?, ?, ?, ?)", (name, start_time, end_time, date))
-    conn.commit()
-    conn.close()
-    return 'Event added successfully!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        subject = request.form['subject']
+        day = request.form['day']
+        time = request.form['time']
+        timetable.append({'subject': subject, 'day': day, 'time': time})
+    return render_template('index.html', subjects=timetable)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
+
 
